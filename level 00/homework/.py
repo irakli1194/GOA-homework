2cd9235a4a8a7a2799e3f1caa0913c5a6f418f9b
iraklizoidze677@gmail.com <iraklizoidze677@gmail.com>
2025-02-25 14:46:35 +0400

    123

---
+++ level 00/homework/.py
@@ -0,0 +1,7 @@
+name="irakli"
+surname="zoidze"
+age=16
+height=181
+
+print("me var" ,name , surname, str(age) , "wlis" , str(height) , "santimetri")
+
---
+++ level 00/homework/lesson1.1.py
@@ -0,0 +1,9 @@
+print(5 + 5)
+
+print(12 - 11)
+
+print(3 * 3)
+
+print(9 / 3)
+
+print(7 % 2)
