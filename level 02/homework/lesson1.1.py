91cf9cd1e5aa4c7a043b59bfe58a70e4d5710304
iraklizoidze677@gmail.com <iraklizoidze677@gmail.com>
2025-03-31 20:00:53 +0400

    burger

--- level 02/homework/lesson1.1.py
+++ level 02/homework/lesson1.1.py
@@ -7,3 +7,23 @@ print(3 * 3)
 print(9 / 3)
 
 print(7 % 2)
+
+
+name="irakli"
+last_name="zoidze"
+age=16
+
+
+a="123"
+b="132"
+c='231'
+
+d=123
+e=213
+f=432
+
+v=1,23
+g=54,23
+o=1,23
+
+print(a,b,c,d,e,f,v,g,o)
